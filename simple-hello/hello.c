532317f69b5d4a26eb20353d4b8e8747490341a4
jespeste <jespersteinsrud1611@gmail.com>
2025-07-03 17:06:19 +0200

    Hello is now updated to have a button

--- simple-hello/hello.c
+++ simple-hello/hello.c
@@ -1,20 +1,33 @@
 #include <gtk/gtk.h>
 
-static void on_activate(GtkApplication *app, gpointer user_data) {
-    GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Hello GTK4");
-    gtk_window_set_default_size(GTK_WINDOW(window), 200, 100);
+static void print_hello (GtkWidget *widget, gpointer data) {
+    g_print("Hello World\n");
+}
 
-    GtkWidget *label = gtk_label_new("Hello World!");
-    gtk_window_set_child(GTK_WINDOW(window), label);
+static void activate(GtkApplication *app, gpointer user_data) {
+
+    GtkWidget *window;  
+    GtkWidget *button;
+
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Hello");
+    gtk_window_set_default_size(GTK_WINDOW(window),200,200);
+
+
+    button = gtk_button_new_with_label("Hello there");
+    g_signal_connect(button, "clicked", G_CALLBACK(print_hello),NULL);
+    gtk_window_set_child(GTK_WINDOW(window),button);
 
     gtk_window_present(GTK_WINDOW(window));
 }
 
 int main(int argc, char **argv) {
-    GtkApplication *app = gtk_application_new("com.example.Hello", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
-    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new("com.example.Hello", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+    status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
 
     return status;
